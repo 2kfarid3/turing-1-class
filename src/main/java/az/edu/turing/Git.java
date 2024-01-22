7f87b2cc0978ed3cafa6520e974e5815e2fe540f
Farid <fkarimli047@gmail.com>
2024-01-22 13:57:51 +0400

    Unversioned Files attached

---
+++ src/main/java/az/edu/turing/Git.java
@@ -0,0 +1,7 @@
+package az.edu.turing;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.print("1\n2\n3\n4\n5");
+    }
+}
---
+++ src/main/java/lesson1/firstlesson.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class firstlesson {
+    public static void main(String[] args) {
+        System.out.print("1\n2\n3\n4\n5");
+    }
+}
